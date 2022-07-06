0086310154d1fee25a7d3e4d83f23ec10a54b782
Adeoye Adefemi Opeoluwa <adefemiadeoye@yahoo.com>
2022-07-06 09:51:37 +0100

    FEAT -> added rocket web framework

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,43 @@
-#[macro_use]
-extern crate dotenv_codegen;
-extern crate dotenv;
-use dotenv::dotenv;
-// use std::env;
+// #[macro_use]
+// extern crate dotenv_codegen;
+// extern crate dotenv;
+// extern crate rocket;
+// use dotenv::dotenv;
+
+// //the base url of the website
+// // #[get("/<name>/<age>")]
+// // fn hello(name: String, age: u8) -> String {
+// //     format!("Hello, {} year old named {}!", age, name)
+// // }
+// #[get("/")]
+// fn index() -> &'static str {
+//     "Hello, world!"
+// }
+
+// #[launch]
+// fn rocket() -> _ {
+//     rocket::build().mount("/", routes![index])
+// }
+// fn main() {
+//     dotenv().ok();
+//     println!("Parsed environment variables");
+//     let url = dotenv!("DATABASE_URL");
+//     let builder = mysql::OptsBuilder::from_opts(mysql::Opts::from_url(&url).unwrap());
+//     let pool = mysql::Pool::new(builder.ssl_opts(mysql::SslOpts::default())).unwrap();
+//     let _conn = pool.get_conn().unwrap();
+//     println!("Successfully connected to PlanetScale!");
+//     rocket::ignite().mount("/hello", routes![hello]).launch();
+// }
 
 
-fn main() {
-    dotenv().ok();
-    println!("Parsed environment variables");
-  
+#[macro_use] extern crate rocket;
 
-    // let url = env::var("DATABASE_URL").expect("DATABASE_URL not found");
-    let url = dotenv!("DATABASE_URL");
-    let builder = mysql::OptsBuilder::from_opts(mysql::Opts::from_url(&url).unwrap());
-    let pool = mysql::Pool::new(builder.ssl_opts(mysql::SslOpts::default())).unwrap();
-    let _conn = pool.get_conn().unwrap();
-    println!("Successfully connected to PlanetScale!");
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
