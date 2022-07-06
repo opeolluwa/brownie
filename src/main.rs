56139117ad63df738d5e875f480deec0cb944dec
Adeoye Adefemi Opeoluwa <adefemiadeoye@yahoo.com>
2022-07-06 22:46:18 +0100

    UPDATE

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,20 @@
 extern crate rocket; // rocket is a framework for building web applications with Rust.
 use rocket_dyn_templates::Template; //for parsing of templates
 mod routes; //import the route handlers from the routes module
-
-//mount the route handlers from the routes module
+            //import all route handlers from the routes module
+            //mount the route handlers from the routes module
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![routes::index,])
-        .mount("/auth", routes![routes::login, routes::sign_up])
+        .mount(
+            "/",
+            //the views are in the templates base dir
+            routes![
+                routes::views::index,
+                routes::views::login,
+                routes::views::sign_up
+            ],
+        ) 
+        .mount("/auth", routes![routes::auth::login, routes::auth::sign_up])
         .attach(Template::fairing())
 }
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,19 +1,45 @@
-//definition of all route handlers
-use rocket_dyn_templates::{context, Template};
-//the home page
-#[get("/")]
-pub fn index() -> Template {
-    Template::render("index", context! { /* name:"drizzle" */ })
+pub mod views {
+    //definition of all route handlers
+    use rocket_dyn_templates::{context, Template};
+    //the home page
+    #[get("/")]
+    pub fn index() -> Template {
+        Template::render("index", context! { /* name:"drizzle" */ })
+    }
+
+    //the login page accessible only to unauthenticated users via /auth/login
+    #[get("/login")]
+    pub fn login() -> Template {
+        Template::render("login", context! { /* name:"drizzle"  */})
+    }
+
+    //the sign up page accessible only to unauthenticated users via /auth/signup
+    #[get("/signup")]
+    pub fn sign_up() -> Template {
+        Template::render("signup", context! { /* name:"drizzle"  */})
+    }
 }
 
-//the login page accessible only to unauthenticated users via /auth/login
-#[get("/login")]
-pub fn login() -> Template {
-    Template::render("auth/login", context! { /* name:"drizzle"  */})
+pub mod auth {
+    use rocket_dyn_templates::{context, Template};
+    //the login page accessible only to unauthenticated users via /auth/login
+    #[post("/login")]
+    pub fn login() -> Template {
+        Template::render("auth/login", context! { /* name:"drizzle"  */})
+    }
+
+    //the sign up page accessible only to unauthenticated users via /auth/signup
+    #[post("/signup")]
+    pub fn sign_up() -> String {
+        "sign up".to_string()
+    }
 }
 
-//the sign up page accessible only to unauthenticated users via /auth/signup
-#[get("/signup")]
-pub fn sign_up() -> String {
-    "sign up".to_string()
+pub mod dashboard {
+    use rocket_dyn_templates::{context, Template};
+    //the login page accessible only to unauthenticated users via /auth/login
+    #[get("/dashboard")]
+    pub fn dashboard() -> Template {
+        Template::render("dashboard/index", context! { /* name:"drizzle"  */})
+    }
 }
