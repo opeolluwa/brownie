224c5c79d074a8660b442976417f41698d5234ea
Adeoye Adefemi Opeoluwa <adefemiadeoye@yahoo.com>
2022-07-06 11:16:34 +0100

    UPDATES

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 #[macro_use]
 extern crate rocket;
+use rocket_dyn_templates::{ Template};
 
-// Try visiting:
-//   http://127.0.0.1:8000/hello/world
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
+
+//import the route handlers from the routes module
+mod routes;
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![routes::index,])
+        .mount("/auth", routes![routes::login, routes::sign_up])
+        .attach(Template::fairing())
 }
