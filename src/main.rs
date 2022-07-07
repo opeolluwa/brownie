ff2bbe8b2172d8c3f27a3427e4f12b37e2731814
Adeoye Adefemi Opeoluwa <adefemiadeoye@yahoo.com>
2022-07-07 13:41:46 +0100

    UPDATES

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@
 extern crate rocket; // rocket is a framework for building web applications with Rust.
 use rocket_dyn_templates::Template; //for parsing of templates
 mod routes; //import the route handlers from the routes module
-            //import all route handlers from the routes module
-            //mount the route handlers from the routes module
+
+//static files 
+use rocket_contrib::serve::StaticFiles;
 
 //import database config
 // use rocket_contrib::databases::diesel;
@@ -24,6 +25,7 @@ fn rocket() -> _ {
         )
         //the authentication endpoints
         .mount("/auth", routes![routes::auth::login, routes::auth::sign_up])
+        .mount("/public", StaticFiles::from("/static")) //static files
         .attach(Template::fairing()) //template engines
         // .attach(RustlyDatastore::fairing()) //database connection
 }
