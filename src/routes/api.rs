632d425f6158f6d51f62b7fff378dbc8f8cc6a89
Adeoye Adefemi Opeoluwa <adefemiadeoye@yahoo.com>
2022-07-10 00:10:31 +0100

    BUG -> errror connection gdb

--- src/routes/api.rs
+++ src/routes/api.rs
@@ -1,21 +1,21 @@
 use crate::RustlyDatastore;
 // use mysql::serde::Serialize;
-// use nanoid::nanoid;
+use nanoid::nanoid;
 use rocket::serde::json::Json;
-// use rocket_db_pools::sqlx;
-use rocket::serde::{Deserialize, Serialize};
+use rocket::serde::Serialize;
+use rocket_db_pools::sqlx;
 use rocket_db_pools::Connection;
 
 #[derive(Responder, Debug, Serialize)]
-struct ApiResponse {
+pub struct ApiResponse {
     status: String,
     // message: String,
 }
 
 #[post("/v1/links/minify", format = "application/json", data = "<raw_url>")]
-pub fn minify(/* mut database: Connection<RustlyDatastore>,  */ raw_url: String,) -> Json<String> {
+pub fn minify(mut database: Connection<RustlyDatastore>, raw_url: String) -> Json<ApiResponse> {
     println!("{}", raw_url);
-    /* let raw_url = raw_url.parse::<String>().unwrap();
+    let raw_url = raw_url.parse::<String>().unwrap();
     let url_id = nanoid!(6);
     let last_modified = String::from("2020-01-01");
     let total_views = 0;
@@ -27,14 +27,14 @@ pub fn minify(/* mut database: Connection<RustlyDatastore>,  */ raw_url: String,
         raw_url.0,
         last_modified,
         total_views
-    ); */
-    /* let response = ApiResponse {
+    ); 
+    let response = ApiResponse {
         status: "success".to_string(),
         // message: "minified url".to_string(),
         // payload: { minified_url:  "http://localhost:8000".to_string() },
-    }; */
-    // Json(response)
-    Json(raw_url)
+    };
+    Json(response)
+    // Json(raw_url)
 }
 
 //to retrieve the minified url from the database then redirect them to the original url
