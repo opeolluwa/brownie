8ae60dd866ac83a6c05c442eac2815c8c855a78c
Adeoye Adefemi Opeoluwa <adefemiadeoye@yahoo.com>
2022-07-11 14:45:59 +0100

    FIXES

--- src/routes/auth.rs
+++ src/routes/auth.rs
@@ -1,7 +1,7 @@
 use crate::models::users::{ApiResponse, User};
 use crate::RustlyDatastore;
-use rocket::serde::{json::Json};
-use rocket_db_pools::{Connection};
+use rocket::serde::json::Json;
+use rocket_db_pools::Connection;
 use rocket_dyn_templates::{context, Template};
 
 //the login page accessible only to unauthenticated users via /auth/login
@@ -12,12 +12,13 @@ pub fn login() -> Template {
 
 //the sign up page accessible only to unauthenticated users via /auth/signup
 #[post("/signup", data = "<user>", format = "application/json")]
-pub fn sign_up(
-    mut database: Connection<RustlyDatastore>,
+pub async fn sign_up(
+    database: Connection<RustlyDatastore>,
     user: Json<User>,
-) -> Option<ApiResponse<()>> {
-    // let new_user = User::new(database, Json(user));
+) -> Json<ApiResponse<()>> {
+    let new_user = User::new(database, user.into_inner()).await;
     // "sign up".to_string()
+    println!("{:?}", new_user);
     todo!()
     /*  ApiResponse {
         success: todo!(),
