f7ccab1382907345e5361f479564ba755d2448dd
Adeoye Adefemi Opeoluwa <adefemiadeoye@yahoo.com>
2022-07-13 01:14:19 +0100

    UPDATE

--- src/routes/views.rs
+++ src/routes/views.rs
@@ -1,11 +1,12 @@
+use chrono::Datelike;
 use rocket_dyn_templates::{context, Template};
 //the home page
 #[get("/")]
 pub fn index() -> Template {
-    // "hello world"
+    let current_year = chrono::Utc::now().year();
     Template::render(
         "index",
-        context! { title:"Rustly - minimal URL shortener"  },
+        context! { title:"Rustly - minimal URL shortener", current_year  },
     )
 }
 
